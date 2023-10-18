aea7a12113b79bf19435a5faedc736cb376cca06
nguyenbavukhoa <nguyenbavukhoa@gmail.com>
2023-10-18 11:06:57 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-
 // Auto hide header on scroll
 const headerNav = document.querySelector(".header-bottom");
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
-    if(lastScrollY < window.scrollY) {
+    if (lastScrollY < window.scrollY) {
         headerNav.classList.add("hide")
     } else {
         headerNav.classList.remove("hide")
     }
     lastScrollY = window.scrollY;
 })
+
+// Auto switch main-wrapper
+var counter = 1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 5000);
