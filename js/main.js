2239a0b1f017fb57de293a90111f9cf33d18695a
Nguyen Ba Vu Khoa <42162681+nguyenbavukhoa@users.noreply.github.com>
2023-11-03 19:07:18 +0700

    update

--- js/main.js
+++ js/main.js
@@ -28,19 +28,19 @@ function uploadProduct(showProduct) {
     // add data products to HTML
     let productHtml = '';
     let listProducts = document.querySelector('.main-menu');
-    if(showProduct.length == 0) {
+    if (showProduct.length == 0) {
         document.getElementById("home-title").style.display = "none";
         productHtml = `<div class="no-result"><div class="no-result-h">Tìm kiếm không có kết quả</div><div class="no-result-p">Xin lỗi, chúng tôi không thể tìm được kết quả hợp với tìm kiếm của bạn</div><div class="no-result-i"><i class="fa-light fa-face-sad-cry"></i></div></div>`;
     } else {
-        let products = showProduct.map(function(product) 
-        {
+        let products = showProduct.map(function (product) {
             return `
-            <a href="productDetail.html?id=${product.id}" class="item">
             <div class="product-preview">
-                <img class="thumbnail" src="${product.img}">
+                <a href="productDetail.html?id=${product.id}" class="item">
+                    <img class="thumbnail" src="${product.img}">
+                </a>    
                 <div class="product-info">
                     <p class="product-title">
-                    ${product.title}
+                        ${product.title}
                     </p>
                     <p class="product-price">${product.price} &#8363;</p>
                     <div class="center">
@@ -51,15 +51,15 @@ function uploadProduct(showProduct) {
                     </div>
                 </div>
             </div>
-            </a>
+            
             `;
-           
+
         });
-        document.getElementById("home-title").style.display = ""; 
-        productHtml = `<section class="product-grid">${products.join('')}</section><section style`;  
+        document.getElementById("home-title").style.display = "";
+        productHtml = `<section class="product-grid">${products.join('')}</section><section style`;
     }
     listProducts.innerHTML = productHtml;
-       
+
 }
 
 window.onload = uploadProduct(JSON.parse(localStorage.getItem('products')));
@@ -69,7 +69,7 @@ function searchProducts() {
     let result = productAll;
     let valueSearchInput = document.querySelector('.form-search-input').value;
     result = valueSearchInput == "" ? result : result.filter(item => {
-        return item.title.toString().toUpperCase().includes(valeSearchInput.toString().toUpperCase());
+        return item.title.toString().toUpperCase().includes(valueSearchInput.toString().toUpperCase());
     })
     console.log(result);
     uploadProduct(result);
